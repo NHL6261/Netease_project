aa074cbcbdfe7362890897c83323405e36c58223
NHL6261 <248376261@qq.com>
2020-02-18 12:41:56 +0800

    底部交互完成

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="footer">
-      <div @click="goFooter('/mister')" class="footerItem">
+      <div :class="{active:$route.path === '/mister'}" @click="goFooter('/mister')" class="footerItem">
           <i class="iconfont icon-caidan06"></i>
           <span>首页</span>
       </div>
-      <div @click="goFooter('/classer')" class="footerItem">
+      <div :class="{active:$route.path === '/classer'}" @click="goFooter('/classer')" class="footerItem">
           <i class="iconfont icon-fenlei"></i>
           <span>分类</span>
       </div>
-      <div @click="goFooter('/meriter')" class="footerItem">
+      <div :class="{active:$route.path === '/meriter'}" @click="goFooter('/meriter')" class="footerItem">
           <i class="iconfont icon-chuanghu"></i>
           <span>值得买</span>
       </div>
-      <div @click="goFooter('/shopping')" class="footerItem">
+      <div :class="{active:$route.path === '/shopping'}" @click="goFooter('/shopping')" class="footerItem">
           <i class="iconfont icon-gouwuche"></i>
           <span>购物车</span>
       </div>
-      <div @click="goFooter('/seul')" class="footerItem">
+      <div :class="{active:$route.path === '/seul'}" @click="goFooter('/seul')" class="footerItem">
           <i class="iconfont icon-geren"></i>
           <span>个人</span>
       </div>
