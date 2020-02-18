a82dd0d96d52648c76ac67364e548e95e533f1f3
NHL6261 <248376261@qq.com>
2020-02-18 22:16:31 +0800

    横向滑屏

---
+++ src/components/Rotation/Rotation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="Rotation">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style lang="stylus">
+ #Rotation
+   width 100%
+   height 
+ 
+</style>
--- src/page/Mister/mister.vue
+++ src/page/Mister/mister.vue
@@ -1,42 +1,70 @@
 <template>
   <div id="Container">
-      <div class="header">
-        <div class="login">
-          <img class="imgItem" src="../../common/images/nav/logo.png" alt="">
-          <div class="searchItem">
-            <i class="iconfont icon-sousuo"></i>
-            <span>搜索商品, 共20423款好物</span>
-          </div>
-          <button class="btn">登录</button>
+    <div class="header">
+      <div class="login">
+        <img class="imgItem" src="../../common/images/nav/logo.png" alt="" />
+        <div class="searchItem">
+          <i class="iconfont icon-sousuo"></i>
+          <span>搜索商品, 共20423款好物</span>
         </div>
-        <div ></div>
+        <button class="btn">登录</button>
       </div>
+      <!-- 橡皮筋效果 -->
+      <div class="wrapper">
+        <ul class="content">
+          <li>推荐</li>
+          <li>居家生活</li>
+          <li>服饰鞋包</li>
+          <li>美食酒水</li>
+          <li>个护清洁</li>
+          <li>母婴亲子</li>
+          <li>运动旅行</li>
+          <li>数码家电</li>
+          <li>全球特色</li>
+        </ul>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Bscroll from 'better-scroll'
   export default {
+    components:{
+
+    },
+    mounted(){
+        new Bscroll('.wrapper',{
+            scrollX:true  //横向滑屏
+        })
+    }
   }
 </script>
 
 <style lang="stylus">
-  #Container
-    width 100%
-    height 100%
+#Container
+  width 100%
+  height 2000px
+ .header
+    width 750px
+    height 148px
+    position fixed
+    left 0
+    top 0
     .login
       display flex
       justify-content space-between
       padding 20px
       box-sizing border-box
       width 100%
-      height 148px
       .imgItem
         width 138px
         height 40px
         margin-top 10px
         vertical-align middle
       .searchItem
-        width 442px  
+        width 442px
         height 56px
         border-radius 10px
         text-align center
@@ -47,7 +75,7 @@
           width 28px
           height 28px
           font-size 40px
-        span 
+        span
           width 317px
           height 42px
           font-size 26px
@@ -63,6 +91,19 @@
         vertical-align middle
         background #fff
         color #DD1A21
-    
- 
+        outline: none
+    .wrapper
+        width 660px 
+        height 60px
+        line-height 60px
+        display flex
+        overflow hidden
+        .content
+          width 1500px
+          display flex
+          text-align center
+          li
+            width 160px 
+            height 60px
+            font-size 28px
 </style>
