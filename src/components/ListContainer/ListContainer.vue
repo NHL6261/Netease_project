026377d35aebf29a9a9c6bf7111a3bd70a5343b4
NHL6261 <248376261@qq.com>
2020-02-19 12:45:51 +0800

    防疫

--- src/components/ListContainer/ListContainer.vue
+++ src/components/ListContainer/ListContainer.vue
@@ -1,93 +1,123 @@
 <template>
   <div id="ListCotainerItem">
-      <div class="ListClasser">
-          <!-- 内容区域 -->
-          <div class="conTainer">
-              <!-- 图片区域 -->
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/1.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/2.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/3.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/4.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/5.png" alt="">
-                  <span>新品首发</span>
-              </div>
-                 <div class="imgItem">
-                  <img src="../../common/images/ListContainer/6.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/7.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/8.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/9.png" alt="">
-                  <span>新品首发</span>
-              </div>
-              <div class="imgItem">
-                  <img src="../../common/images/ListContainer/10.gif" alt="">
-                  <span>新品首发</span>
-              </div>
-          </div>
+    <div class="ListClasser">
+      <!-- 内容区域 -->
+      <div class="conTainer">
+        <!-- 图片区域 -->
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/1.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/2.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/3.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/4.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/5.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/6.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/7.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/8.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/9.png" alt="" />
+          <span>新品首发</span>
+        </div>
+        <div class="imgItem">
+          <img src="../../common/images/ListContainer/10.gif" alt="" />
+          <span>新品首发</span>
+        </div>
       </div>
-      <div class="ListCordon">
+    </div>
+    <div class="ListCordon">
+      <div class="ListImgItme">
+        <img src="../../common/images/Cordon/1.webp" alt="" />
+      </div>
+      <div class="Bck">
+        <div class="ListPrenDre">
+          <img src="../../common/images/Cordon/2.gif" alt="" />
+        </div>
 
       </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-  }
+export default {
+}
 </script>
 
 <style lang="stylus">
-  #ListCotainerItem
+#ListCotainerItem
+  width 100%
+  height 4766px
+  .ListClasser
     width 100%
-    height 4766px
-    .ListClasser
-      width 100%
-      height 372px
-      .conTainer
+    height 372px
+    .conTainer
+      display flex
+      justify-content space-between
+      flex-wrap: wrap
+      padding 30px
+      box-sizing border-box
+      .imgItem
+        width 110px
+        height 156px
         display flex
-        justify-content space-between
-        flex-wrap: wrap
-        padding 30px
-        box-sizing border-box
-        .imgItem
+        flex-direction column
+        margin-right 10px
+        margin-left 10px
+        margin-top 10px
+        img
           width 110px
-          height 156px
-          display flex
-          flex-direction column
-          margin-right 10px
-          margin-left 10px
-          margin-top 10px
-          img 
-            width 110px
-            height 110px
-            text-align center
-          span 
-            width 110px
-            height 36px
-            margin-top 5px
-            text-align center
-            line-height 36px
-            img:nth-child(5)
-                margin-right 0px
+          height 110px
+          text-align center
+        span
+          width 110px
+          height 36px
+          margin-top 5px
+          text-align center
+          line-height 36px
+          img:nth-child(5)
+              margin-right 0px
+  .ListCordon
+      width 100%
+      height 828px
+      .ListImgItme
+          width 100%
+          height 240px
+          img
+              width 100%
+              height 100%
+              padding 5px 1px 0 1px
+              box-sizing border-box
+      .Bck
+        width 100%
+        height 532px        
+        background-color #1674e3
+        .ListPrenDre
+            width 710px
+            height 160px
+            img
+                width 710px
+                height 160px
+                margin-left 20px
 </style>
