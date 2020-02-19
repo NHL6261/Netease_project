76f659ab9854e23fa459101e97411ce21a0a47e1
NHL6261 <248376261@qq.com>
2020-02-19 23:11:46 +0800

    商品列表基本框架完成，得睡觉了

---
+++ src/components/Ardent/Ardent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="ArdentContainer">
+      <!-- 头部 类目热销榜 -->
+      <div class="HeaderItem">
+
+      </div>
+      <!-- 商品列表 -->
+      <div class="ShoppingList">
+          <!-- 商品列表头部 -->
+          <div class="ShoppingHeader">
+              
+          </div>
+          <!-- 商品列表内容 -->
+          <div class="ShoppingContainer">
+              <!-- 图片 -->
+              <div class="ImgItem">
+                  
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style lang="stylus">
+
+ 
+</style>
